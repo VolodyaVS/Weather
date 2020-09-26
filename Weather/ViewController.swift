18c6fc901a03b22d9457cdf3adb789fbab309b60
Volodya <volodyavolga@protonmail.ch>
2020-09-26 11:19:25 +0300

    update VC

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pressureLabel: UILabel!
+    @IBOutlet weak var humidity: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var appearentTempuratureLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    @IBAction func refreshButtonTapped(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
